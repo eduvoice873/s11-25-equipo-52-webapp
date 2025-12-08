a7229d19423248c44a5a1982ad917b9d5ac7195a
eduvoice873 <eduvoice873@gmail.com>
2025-12-08 15:36:24 -0500

    Update useUsers.ts

--- hooks/swr/useUsers.ts
+++ hooks/swr/useUsers.ts
@@ -1,5 +1,6 @@
-import useSWR from "swr";
-import fetcher from "@/lib/fetcher";
+//import useSWR from "swr";
+
+//import fetcher from "@/lib/fetcher";
 
 export function useUsers() {
   const { data, error, isLoading, mutate } = useSWR("/api/users", fetcher);
